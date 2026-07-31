616eb3465ce6ab02dbe4cfaa292f44fb99a1a312
DavidKN10 <davidrs23178@gmail.com>
2026-07-31 00:49:53 -0500

    frontend: added simple NavigationView

--- NativeMetrics/MainWindow.xaml.cs
+++ NativeMetrics/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+
+using NativeMetrics.Views;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +30,35 @@ namespace NativeMetrics
         public MainWindow()
         {
             InitializeComponent();
+          
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(titleBar);
+            
+            // show an initial page
+            ContentFrame.Navigate(typeof(ProcessesPage));
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItemContainer is NavigationViewItem item)
+            {
+                switch(item.Tag?.ToString())
+                {
+                    case "ProcessesPage":
+                        ContentFrame.Navigate(typeof(ProcessesPage));
+                        break;
+                    case "PerformancePage":
+                        ContentFrame.Navigate(typeof(PerformancePage));
+                        break;
+                    case "NetworkPage":
+                        ContentFrame.Navigate(typeof(NetworkPage));
+                        break;
+                    case "SettingsPage":
+                        ContentFrame.Navigate(typeof(SettingsPage));
+                        break;
+                }
+            }
+
         }
     }
 }
